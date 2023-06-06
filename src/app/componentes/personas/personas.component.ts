7b0a336a124c9172bde5a126849c35d60182246b
FranAchard <franachard8@gmail.com>
2023-06-06 10:13:25 -0300

    Primeros pasos

--- src/app/componentes/personas/personas.component.ts
+++ src/app/componentes/personas/personas.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/Persona';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css']
 })
-export class PersonasComponent {
-
+export class PersonasComponent implements OnInit {
+    personas:Persona[]=[]
+    constructor(){}
+    ngOnInit(): void {
+      let persona1= new Persona("Francisco", "Achard", 21)
+      this.personas.push(persona1)
+      this.personas.push(new Persona("Lionel", "Messi", 35))
+    }
 }
